b00a318dbadd3676f3f57cafea0594048eb8656a
rodrigodojo <rodrigodoj@gmail.com>
2021-02-06 16:28:06 -0300

    Atualizando processo

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,25 @@
 package application;
 
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		File file = new File("c:\\temp\\in.txt");
-		Scanner sc = null;
+		String path = "c:\\temp\\out.txt";
+		String[] lines = new String[] {"Good morning","good afternoon","good night"};
 		
-		try {
-			sc = new Scanner(file);
-			while(sc.hasNextLine()) {
-				System.out.println(sc.nextLine());
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+				System.out.println("Gravado");
 			}
-			
 		}
 		catch(IOException e){
-			System.out.println("Error: " +e.getMessage());
-		}
-		finally {
-			if(sc != null) {
-				sc.close();				
-			}
-		}
+			e.printStackTrace();
+		}	
 	}
 }
